0eacb140df95ee9e83692782c9900f696364f0d0
Andrew Dales <andrew.dales@highgateschool.org.uk>
2022-03-15 23:12:55 +0000

    Adding INSERT command

--- main.py
+++ main.py
@@ -7,7 +7,18 @@ cursor = conn.cursor()
 query = "SELECT FirstName, LastName, Title FROM employees"
 cursor.execute(query)
 employees = cursor.fetchall()
-conn.close()
+
 
 # Print out the first employee
 print(employees[0])
+
+# Insert a new genre
+query = 'INSERT INTO genres (Name) values ("Funk")'
+cursor.execute(query)
+# Before a change is written to the database, it must be committed
+conn.commit()
+
+
+# Close the connection at the end
+conn.commit()
+
